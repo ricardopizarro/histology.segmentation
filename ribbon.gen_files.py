e7c3f8e4061857cfbcd98601002dac8ec186d154
Ricardo Pizarro <rap@matador.local>
2018-04-20 11:45:28 -0400

    adding hull list of files

--- ribbon.gen_files.py
+++ ribbon.gen_files.py
@@ -15,18 +15,20 @@ def grab_files(path,end):
     return glob.glob(os.path.join(path,end))
 
 
-def split_cross_valid(slices_fn,segments_fn):
-    # slices_base=[os.path.basename(s) for s in slices_fn]
-    # segments_base=[os.path.basename(s) for s in segments_fn]
+def split_cross_valid(slices_fn,segments_fn,hulls_fn):
 
     all_files =[]
     for n,seg_fn in enumerate(segments_fn):
         slice_fn=difflib.get_close_matches(seg_fn,slices_fn)[0]
+        hull_fn=difflib.get_close_matches(seg_fn,hulls_fn)[0]
         # slice_fn=[s for s in slices_fn if slice_base in s]
         if not slice_fn:
             print("Could not find an equivalent segment file {}".format(segment_fn))
             continue
-        all_files.append((slice_fn,seg_fn))
+        elif not hull_fn:
+            print("Could not find an equivalent hull file {}".format(hull_fn))
+            continue
+        all_files.append((slice_fn,seg_fn,hull_fn))
 
     return all_files
 
@@ -43,16 +45,16 @@ for p in data_path:
     slices_fn += grab_files(p,'*/*jpg.nii')
     segments_fn += grab_files(p,'*/*segmented.nii*')
 
-# print(segments_fn)
+hull_path='/data/shmuel/shmuel1/rap/histo/data/20180420_hull'
+hulls_fn=grab_files(hull_path,'*hull.nii.gz')
 
-all_files = split_cross_valid(slices_fn,segments_fn)
+all_files = split_cross_valid(slices_fn,segments_fn,hulls_fn)
 
-print(all_files)
+# print(all_files)
 
-fn='/data/shmuel/shmuel1/rap/histo/data/segmented_files.txt'
+fn='/data/shmuel/shmuel1/rap/histo/data/segmented_hull_files.txt'
 thefile = open(fn,'w')
-
 for item in all_files:
-    thefile.write('%s\n' % item)
+    thefile.write(' '.join(str(s) for s in item) + '\n')
 
 
