54e4f7af2a9b0f84e25371d7d231cf810b27b8f9
Ricardo Pizarro <rap@matador.local>
2018-04-20 11:02:07 -0400

    convexicating two files first

--- ribbon.BIC_CPU.test.sh
+++ ribbon.BIC_CPU.test.sh
@@ -5,8 +5,8 @@ source /data/shmuel/shmuel1/rap/histo/venv_cpu/bin/activate
 export TF_CPP_MIN_LOG_LEVEL=2
 
 cd /data/shmuel/shmuel1/rap/histo/src
-# python ribbon.test_convex_hull.py
 # python ribbon.inter-rater.py
 slice_fn=$1
 segment_fn=$2
-python ribbon.test_CNN_unet.n2soft.prob.py $slice_fn $segment_fn
+python ribbon.test_convex_hull.py $slice_fn $segment_fn
+# python ribbon.test_CNN_unet.n2soft.prob.py $slice_fn $segment_fn
--- ribbon.mv_qsub.sh
+++ ribbon.mv_qsub.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mv ~/ribbon.BIC_* ~/log_ribn
-segment_files='/data/shmuel/shmuel1/rap/histo/data/segmented_files.txt'
+segment_files='/data/shmuel/shmuel1/rap/histo/data/two_segmented_files.txt'
 while IFS=" " read -r slice_fn segment_fn remainder; do
     qsub -q all.q -l h_vmem=65G ribbon.BIC_CPU.test.sh $slice_fn $segment_fn
 done <$segment_files
